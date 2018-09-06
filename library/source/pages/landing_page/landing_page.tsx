06647160bb94eef0469d79a9f6b766e839ef6523
Sofiya Polyanska <sofiya@spiretrading.com>
2018-09-06 15:02:12 -0400

    Styling of the layout completed for the button and mostly completed for the input box.

--- library/source/pages/landing_page/landing_page.tsx
+++ library/source/pages/landing_page/landing_page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as Router from 'react-router-dom';
 import {VBoxLayout, HBoxLayout, Padding} from '../../layouts';
+import {StyleSheet, css} from 'aphrodite';
 
 /** Specifies the React properties for the landing page. */
 interface Properties {
@@ -40,7 +41,7 @@ export class LandingPage extends React.Component<Properties, State> {
     return (
       <HBoxLayout height='100%' width='100%'>
         <Padding/>
-        <VBoxLayout>
+        <VBoxLayout width='200px'>
           <Padding/>
           <div>
             <img src = 'resources/images/landing_page/sudoku.svg'
@@ -53,14 +54,15 @@ export class LandingPage extends React.Component<Properties, State> {
           <Padding size='60px'/>
           <div style={LandingPage.NAME_STYLE}>YOUR NAME</div>
           <Padding size='8px'/>
-          <input type='text' style={LandingPage.NAME_INPUT_STYLE}/> 
+          <input placeholder = 'Max. of 10 Characters'
+            className={css(LandingPage.NAME_INPUT_STYLE.input)}/>
           <Padding size='40px'/>
           <div>
-            <button style={LandingPage.BUTTON_STYLE}>
+            <button className={css(LandingPage.BUTTON_STYLE.button)}>
               PLAY NOW</button>
           </div>
           <Padding size='40px'/>
-          <div style={LandingPage.STANDINGS_STYLE}>See Standings</div>
+          <a href="" style={LandingPage.STANDINGS_STYLE}>See Standings</a>
           <Padding/>
         </VBoxLayout>
       <Padding/>
@@ -88,27 +90,55 @@ export class LandingPage extends React.Component<Properties, State> {
     textAlign: 'center' as 'center'
   };
 
-  private static readonly BUTTON_STYLE = {
-    height: '56px',
-    width: '100%',
-    boxSizing: 'border-box' as 'border-box',
-    border: '1px solid #4B23A0',
-    borderRadius: '4px',
-    fontFamily: 'Roboto',
-    fontSize: '20px',
-    textAlign: 'center' as 'center',
-    color: '#4B23A0'
-  };
+  private static readonly BUTTON_STYLE = StyleSheet.create({
+    button: {
+      height: '56px',
+      width: '100%',
+      boxSizing: 'border-box' as 'border-box',
+      border: '1px solid #4B23A0',
+      borderRadius: '4px',
+      fontFamily: 'Roboto',
+      fontSize: '20px',
+      textAlign: 'center' as 'center',
+      color: '#4B23A0',
+      background: '#FFFFFF',
+      ':focus': {
+        background: '#F2F2FF',
+      },
+      ':hover': {
+        background: '#4B23A0',
+        color: '#F2F2FF'
+      }
+    }
+  });
 
-  private static readonly NAME_INPUT_STYLE = {
-    height: '40px',
-    padding: '11px',
-    boxSizing: 'border-box' as 'border-box',
-    border: '1px solid #4B23A0',
-    borderRadius: '4px',
-    fontFamily: 'Roboto',
-    fontSize: '16px',
-    textAlign: 'left' as 'left',
-    color: '#333333'
-  };
+  private static readonly NAME_INPUT_STYLE = StyleSheet.create({
+    input: {
+      height: '40px',
+      padding: '11px',
+      boxSizing: 'border-box' as 'border-box',
+      border: '1px solid #A0A0A0',
+      borderRadius: '4px',
+      fontFamily: 'Roboto',
+      fontSize: '16px',
+      textAlign: 'left' as 'left',
+      color: '#333333',
+      type: 'text',
+      ':focus': {
+        border: '1px solid #4B23A0',
+        '::placeholder': {
+          color: '#0A0A0A',
+          visibility:'visible',
+          fontFamily: 'Roboto',
+          fontSize: '16px'
+          }
+      },
+     '::placeholder': {
+        color: 'transparent',
+        visibility:'hidden',
+        fontFamily: 'Roboto',
+        fontSize: '16px'
+      }
+    }
+  });
 }
